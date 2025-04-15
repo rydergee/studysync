ac3a60f15daec89b61abd6d4bc3ad16a5b2083c3
rydergee <ryder.gee2005@gmail.com>
2025-04-16 11:43:05 +1200

    Create ics_parser a method of reading the ics file and extracting all the assignments and work

---
+++ ics_parser.py
@@ -0,0 +1,23 @@
+from icalendar import Calendar
+from datetime import datetime
+def load_ics_events(filepath):
+    with open(filepath, 'r') as f:
+        cal = Calendar.from_ical(f.read())
+
+    events = []
+    for event in cal.walk():
+        if event.name == "VEVENT":
+            dt = event.get("dtstart").dt
+            if(isinstance(dt,datetime) is False):
+                dt = datetime.combine(dt,datetime.max.time().replace(microsecond=0))
+    
+            events.append({
+                "id": None,
+                "summary": str(event.get("summary")),
+                "estimated_time": None,
+                "remaining_time": None,
+                "due": dt,
+                "completed": False,
+                "source": "canvas"
+            })
+    return events
